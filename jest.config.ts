abf04f16c4eade5c3d6fe48cebdc0801265a734d
himynameisdave <dave@fossa.com>
2020-10-21 17:30:44 -0700

    👩‍🔬 Sets up Jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  roots: [
+    '<rootDir>/src'
+  ],
+  'testMatch': [
+    '**/__tests__/**/*.+(ts|js)"',
+    '**/?(*.)+(spec|test).+(ts|js)'
+  ],
+  'transform': {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+}
