ad1e9e5c3cfef9c8493aa10540928641bf97182a
himynameisdave <dave@fossa.com>
2020-10-23 16:35:44 -0700

    🔧 Fix a failing test

--- src/utils/__tests__/path.spec.ts
+++ src/utils/__tests__/path.spec.ts
@@ -7,9 +7,9 @@ describe('utils/path', () => {
       //  NOTE: mocking this out doesn't work, because Jest relies on process.cwd().
       //  See more: https://github.com/facebook/jest/issues/5146
       const cwd = process.cwd();
-      const mockInputPath = 'src/styles/';
+      const mockInputPath = 'src/styles';
       const actual = resolveCwd(mockInputPath);
-      expect(actual).toEqual(`${cwd}/${mockInputPath}/`);
+      expect(actual).toEqual(`${cwd}/${mockInputPath}`);
     });
   });
 });
